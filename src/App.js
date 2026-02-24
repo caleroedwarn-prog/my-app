b885c6cba9667d3bbb0095c2f608728a6e0857a5
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-02-24 23:51:53 +0100

    react simple practice

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div>
       <pre>
       <h1>Hello world I am {name}</h1>
-      <p>I am {age} years old</p>
+      <p>I am {age} years old and will be turning {age + 1} this june 6 2026</p>
       <p>From the {Uni} Cameroon</p>
       <p>This is my first real React Lesson</p>
       </pre>
