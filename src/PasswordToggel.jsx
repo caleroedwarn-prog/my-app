e1025f49f926250a7f8f5984ecc81f791a1b0854
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-02-26 18:56:10 +0100

    Creating the password toggle

--- src/PasswordToggel.jsx
+++
@@ -1,9 +0,0 @@
-function Introduction() {
-    return (
-        <>
-        <p>I am a passionate coder and an innovator</p>
-        <p>I love chalenges and tasks and my consistency starts from now</p>
-        </>
-    )
-}
-export default Introduction;
