730517048e0df5cb4788918d334a0d75602f0b60
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-03-12 01:57:03 +0100

    Creating and updating the login, logout system in Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,11 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-function Navbar(){
+function Navbar({ isLoggedIn, setIsLoggedIn }){
+    const navigate = useNavigate();
+    function handleLogout(){
+        setIsLoggedIn(false);
+        navigate("logins");
+    }
     return(
         <nav style={{display: "flex", gap: "10px"}}>
             <Link to ="/">Home</Link> |
             <Link to="/about">About</Link> |
-            <Link to = "/logins">Login</Link>
+            {isLoggedIn && (
+                <Link to = "/dashbord">Dashboard</Link>
+            )}
+            {!isLoggedIn && (
+                <Link to= "/logins">Login</Link>
+            )}
+            {isLoggedIn && (
+                <button onClick = {handleLogout}>Logout</button>
+            )}
         </nav>
     );
 }
