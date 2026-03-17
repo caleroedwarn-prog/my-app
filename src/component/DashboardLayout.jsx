9f5d5b715bd92fbfb19d86bfd684f986b6c53a5c
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-03-17 22:09:44 +0100

    Creating profile page for dashboard as children component

---
+++ src/component/DashboardLayout.jsx
@@ -0,0 +1,9 @@
+import { Outlet, Link } from "react-router-dom";
+
+function DashboardLayout() {
+    return(
+        <div style={{display:"flex", gap:"20px"}}>
+            
+        </div>
+    );
+}
