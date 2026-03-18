7358423f6a4a529742b1c69c7792fe610883d2fa
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-03-18 12:04:35 +0100

    showing user on screan mapping each user from users

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,12 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 function Users(){
+    const [users, setUsers] = useState([]);
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/users").then(response => response.json()).then(data => {
             console.log(data);
+            setUsers(data)
+
         });
     }, []);
     return (
+        <>
         <h3>User page</h3>
+        { users.map(user => (
+            <p key={user.id}>
+            { user.name }
+            {user.email}
+            </p>
+        ))}
+        </>
     );
 }
 export default Users;
