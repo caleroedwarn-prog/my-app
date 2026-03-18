1cbaa2cbe2522fad01ff0c7c096ca0858199f726
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-03-18 07:42:02 +0100

    Created a user page for fetch API

--- src/component/DashboardLayout.jsx
+++ src/component/DashboardLayout.jsx
@@ -8,6 +8,7 @@ function DashboardLayout() {
                 <Link to= "profile">Profile</Link>
                 <Link to="setting">Settings</Link>
                 <Link to="stats">Stats</Link>
+                <Link to="user">Users</Link>
             </div>
             <Outlet />
         </div>
