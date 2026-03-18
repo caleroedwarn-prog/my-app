414604add427dc1eec63ec7505754cf6b0323438
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-03-18 07:50:44 +0100

    using fetch( ) with a fake server just to test if it works in the console

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
 function Users(){
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users").then(response => response.json()).then(data => {
+            console.log(data);
+        });
+    }, []);
     return (
         <h3>User page</h3>
     );
