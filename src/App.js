ec27f65084c5d19ec7f11190f35deb2ace8a5baa
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-03-18 07:38:33 +0100

    Updated the error page into app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState } from "react";
 import Homes from "./pages/Homes";
 import About from "./pages/About";
@@ -11,6 +11,7 @@ import DashboardLayout from "./component/DashboardLayout";
 import Profile from "./pages/Profile";
 import Settings from "./pages/Setting";
 import Stats from "./pages/Stats";
+import NotFound from "./pages/NotFound";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
@@ -21,20 +22,24 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />} />
           <Route index element={<Homes />} />
-          <Route path="about" element={<About />} />     
-          <Route path="logins" element={<Logins setIsLoggedIn={setIsLoggedIn} />} />
-         <Route 
-         path="dashboard"
-         element={
-          <ProtectedRoute isLoggedIn={isLoggedIn}>
-            <DashboardLayout />
-          </ProtectedRoute>
-         } 
-         >
-         <Route path="profile" element={<Profile />} />
-         <Route path="setting" element={<Settings />} />
-         <Route path="stats" element={<Stats />} />
-         </Route>
+          <Route path="about" element={<About />} />
+          <Route
+            path="logins"
+            element={<Logins setIsLoggedIn={setIsLoggedIn} />}
+          />
+          <Route
+            path="dashboard"
+            element={
+              <ProtectedRoute isLoggedIn={isLoggedIn}>
+                <DashboardLayout />
+              </ProtectedRoute>
+            }
+          >
+            <Route path="profile" element={<Profile />} />
+            <Route path="setting" element={<Settings />} />
+            <Route path="stats" element={<Stats />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
       <br />
