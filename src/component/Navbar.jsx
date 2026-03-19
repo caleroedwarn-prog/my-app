3607d2cdcd7228862e990fdcd22268649f2f230f
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-03-19 21:42:00 +0100

    updated

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -10,6 +10,7 @@ function Navbar({ isLoggedIn, setIsLoggedIn }){
         <nav style={{display: "flex", gap: "10px"}}>
             <Link to ="/">Home</Link> |
             <Link to="/about">About</Link> |
+            <Link to="/register">Register</Link>|
             {isLoggedIn && (
                 <Link to = "dashboard">Dashboard</Link>
             )}
