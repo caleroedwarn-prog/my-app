3d96831cc9a585c71c7c883873c611c6a2be52cb
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-03-19 07:07:21 +0100

    using axios to get users data

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,24 +1,26 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 function Users(){
     const [users, setUsers] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null); 
 
     useEffect(() => {
-
         async function getUsers(){
+
             try{
-                const res = await fetch("https://jsonplaceholder.typicode.com/users");
-                const data = await res.json();
-                setUsers(data);
-            }catch(err){
-                setError("Failed to Load Users")
-            }finally{
-                setLoading(false);
-            }
+            const res = await  axios.get("https://jsonplaholder.typicode.com/users");
+
+            setUsers(res.data);
+            
+        }catch(err){
+            setError("Failed to load users");
+        }finally{
+            setLoading(false);
         }
-        getUsers();
-    },[])
+    }
+    getUsers();
+}, [])
 
     if(loading){
         return <h2>Loading user ...</h2>
