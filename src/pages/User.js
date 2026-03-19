aea04ec500ce96ffd910448e3a3d3cce05b21d61
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-03-19 01:55:50 +0100

    updated and modified it by adding error handling

--- src/pages/User.js
+++ src/pages/User.js
@@ -3,14 +3,29 @@ function Users(){
     const [users, setUsers] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null); 
+
     useEffect(() => {
+
         async function getUsers(){
-            const res = await fetch("https://jsonplaceholder.typicode.com/users");
-            const data = await res.json();
-            setUsers(data)
+            try{
+                const res = await fetch("https://jsonplaceholder.typicode.com/users");
+                const data = await res.json();
+                setUsers(data);
+            }catch(err){
+                setError("Failed to Load Users")
+            }finally{
+                setLoading(false);
+            }
         }
         getUsers();
-    }, []);
+    },[])
+
+    if(loading){
+        return <h2>Loading user ...</h2>
+    }
+    if (error){
+        return <h2>{error}</h2>
+    }
     return (
         <>
         <h3>User page</h3>
