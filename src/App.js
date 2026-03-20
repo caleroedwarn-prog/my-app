1df2ead0c820927008215b49025bee2c1d92feb6
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-03-20 02:46:56 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useState, lazy, suspense } from "react";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { useState, lazy, Suspense } from "react";
 import Homes from "./pages/Homes";
 import About from "./pages/About";
-import Logins from "./pages/Login";
 import Navbar from "./component/Navbar";
 import ProtectedRoute from "./component/ProtectedRoute";
-import Dashboard from "./pages/Dashboard";
 import Layout from "./component/Layout";
 import DashboardLayout from "./component/DashboardLayout";
 import Profile from "./pages/Profile";
@@ -20,34 +18,36 @@ function App() {
   return (
     <div>
       <br />
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Layout />} />
-          <Route index element={<Homes />} />
-          <Route path="about" element={<About />} />
-          <Route
-            path="logins"
-            element={<Logins setIsLoggedIn={setIsLoggedIn} />}
-          />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="dashboard"
-            element={
-              <ProtectedRoute isLoggedIn={isLoggedIn}>
-                <DashboardLayout />
-              </ProtectedRoute>
-            }
-          >
-            <Route path="profile" element={<Profile />} />
-            <Route path="setting" element={<Settings />} />
-            <Route path="stats" element={<Stats />} />
-            <Route path="user" element={<Users />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-      <br />
+      <Suspense fallback={<h>Loading..</h>}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Layout />} />
+            <Route index element={<Homes />} />
+            <Route path="about" element={<About />} />
+            <Route
+              path="logins"
+              element={<Login setIsLoggedIn={setIsLoggedIn} />}
+            />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="dashboard"
+              element={
+                <ProtectedRoute isLoggedIn={isLoggedIn}>
+                  <DashboardLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route path="profile" element={<Profile />} />
+              <Route path="setting" element={<Settings />} />
+              <Route path="stats" element={<Stats />} />
+              <Route path="user" element={<Users />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+        <br />
+      </Suspense>
     </div>
   );
 }
