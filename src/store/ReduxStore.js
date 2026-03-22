92c2dd64b7f2a22d2cbce94979b3a9ee43299802
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-03-22 14:53:02 +0100

    update with authslice

--- src/store/ReduxStore.js
+++ src/store/ReduxStore.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./authslice";
 
 const Store = configureStore({
     reducer: {
-
+        auth : authReducer
     }
 });
  export default Store;
