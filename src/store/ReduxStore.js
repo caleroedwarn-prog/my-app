8559d8485ccc8e73e4cfbc81151662dd84b5ac93
caleroedwarn-prog <caleroedwarn@gmail.com>
2026-03-22 14:20:23 +0100

    modification

--- src/store/ReduxStore.js
+++ src/store/ReduxStore.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-const store = configureStore({
+const Store = configureStore({
     reducer: {
 
     }
 });
- export default Store
+ export default Store;
